2fad71d39603da5d28a1624eb21eef4876793e46
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-05-31 11:00:22 +0500

    calculate two number

--- ForSmallProblems/Program.cs
+++ ForSmallProblems/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Resources;
+using System.Runtime.Intrinsics.Arm;
 
 namespace ForSmallProblems
 {
@@ -6,7 +8,17 @@ namespace ForSmallProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            string[] arr = Console.ReadLine().Split();
+            Console.WriteLine(arr[1] + " " + arr[0]);
+            foreach (string s in arr)
+            {
+                sum += Convert.ToInt32(s);
+            }
+            Console.WriteLine(sum);
+
+
+
         }
     }
 }
