fded0c8db2d67a4030a120f39b6769238b3e3f4e
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-06-07 07:30:27 +0500

    added new problems

--- ForSmallProblems/Program.cs
+++ ForSmallProblems/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
-using System.Resources;
-using System.Runtime.Intrinsics.Arm;
-using System.Runtime.Versioning;
+using System.Buffers;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace ForSmallProblems
 {
@@ -9,6 +12,10 @@ namespace ForSmallProblems
     {
         static void Main(string[] args)
         {
+
+
+
+
             //int sum = 0;
             //string[] arr = Console.ReadLine().Split();
             //Console.WriteLine(arr[1] + " " + arr[0]);
@@ -60,7 +67,7 @@ namespace ForSmallProblems
 
 
             //int count = secondNumber%2 == 0 ? 4 : 5;
-            
+
             //do
             //{
             //    if (secondNumber - count >= firstNumber)
@@ -85,8 +92,127 @@ namespace ForSmallProblems
             //} while (count >= 0);
 
 
+            //=========================
+
+            //int[] number = { 1, 2, 3 };
+            //int[] number2 = { 1, 2, 3 };
+            //int[] number3 = new int[3];
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    number3[i] = number[i] + number2[i];
+            //}
+            //Array.ForEach(number3, Console.WriteLine);
+
+            //int reverseNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine();
+
+            //======================================
+
+            //string num = Console.ReadLine();
+            //char[] reverseNum = new char[num.Length];
+
+            //for(int i = 0; i< num.Length; i++)
+            //{
+            //    reverseNum[i] = num[i];
+            //}
+            //Array.Reverse(reverseNum);
+            //int gum = int.Parse(new string(reverseNum));
+            //Console.WriteLine(gum);
+
+            //==============================================
+
+            //int[] ar = { 1, 2, 3, 5, 5 };
+            //int[] num = TwoSum(ar, 5);
+            //Array.ForEach(num,Console.WriteLine);
+
+            //=============================
+            // palindrome number
+
+            //int number = int.Parse(Console.ReadLine());
+            //int rem = 0;
+            //int number2 = number;
+
+            //while (number != 0)
+            //{
+            //    int num = number % 10;
+            //    rem = rem * 10 + num;
+            //    number = number / 10;
+            //}
+
+            //if(number2 == rem)
+            //    Console.WriteLine(true);
+            //else
+            //    Console.WriteLine(false);
+            //====
+            //string str = Console.ReadLine();
+
+            //char[] strArr = new char[str.Length];
+
+            //for (int i = 0; i < strArr.Length; i++)
+            //{
+            //    strArr[i] = str[i];
+            //}
+            //Array.Reverse(strArr);
+            //string str2 = new string(strArr);
+            //if (str ==  str2)
+            //    Console.WriteLine(true);
+            //else
+            //    Console.WriteLine(false);
+
+            //I             1
+            //V             5
+            //X             10
+            //L             50
+            //C             100
+            //D             500
+            //M             1000
+            // IV = 4 IX = 9 XL = 40 XC = 90 CD = 400 CM = 900 
+            //string strnum = Console.ReadLine();
+
+            //char[] str = new char[] { };
+            //for(int i = 0; i < strnum.Length; i++)
+            //{
+            //    str[i] = strnum[i];
+            //}
+
+            //int i = 0;
+            //StringBuilder justString = new StringBuilder();
+            //int number=0;
+            //while (i!=strnum.Length)
+            //{
+
+            //    switch (i)
+            //    {
+            //        case 'M':
+            //            break;
+            //    }
+            //}
+
+
+
+
+            Console.WriteLine("Hello World!");
 
 
         }
+        //public static int[] TwoSum(int[] nums, int target)
+        //{
+
+        //    int[] output = new int[2];
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        for (int j = 0; j < nums.Length; j++)
+        //        {
+        //            if (nums[i] + nums[j] == target)
+        //            {
+        //                output[0] = i;
+        //                output[1] = j;
+        //            }
+
+        //        }
+        //    }
+        //    return output;
+
+        //}
     }
 }
